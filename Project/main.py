6631c139e1cf13b9f79191026e642f121109c4c9
dimitrisstyl7 <dimitrisstylianou@proton.me>
2024-02-17 19:17:59 +0200

    Create Project\FeatureVectorMethods.py #1
    - Create extract_feature_vectors method.
    - Create get_pretrained_model method.
    
    Co-Authored-By: Niki Dim <79726656+Nikidmp@users.noreply.github.com>
    Co-Authored-By: panagiota02 <79789822+panagiota02@users.noreply.github.com>

--- Project/main.py
+++ Project/main.py
@@ -1,8 +1,12 @@
 from DatasetMethods import *
+from FeatureVectorMethods import *
 if __name__ == '__main__':
     # Load the dataset
     dataset = load_dataset()
 
     # Create a subset of the dataset
     subset_dataset, subset_indices, target_indices = get_subset_dataset(dataset)
+
+    # Extract the feature vectors for all images in the subset dataset
+    feature_vectors, target_feature_vectors = extract_feature_vectors(subset_dataset, target_indices)
 
