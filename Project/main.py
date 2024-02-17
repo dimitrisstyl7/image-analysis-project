58db9f4cd86afe4deb9e0546a66ad5553bbdff1b
dimitrisstyl7 <dimitrisstylianou@proton.me>
2024-02-17 19:16:22 +0200

    Create Project\DatasetMethods.py #1
    - Create load_dataset method.
    - Create get_subset_dataset method.
    
    Co-Authored-By: Niki Dim <79726656+Nikidmp@users.noreply.github.com>
    Co-Authored-By: panagiota02 <79789822+panagiota02@users.noreply.github.com>

--- Project/main.py
+++ Project/main.py
@@ -1,2 +1,8 @@
+from DatasetMethods import *
 if __name__ == '__main__':
-    pass
+    # Load the dataset
+    dataset = load_dataset()
+
+    # Create a subset of the dataset
+    subset_dataset, subset_indices, target_indices = get_subset_dataset(dataset)
+
