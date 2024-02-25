28d5f83b007e534629ecef30312bca94af9031a7
dimitrisstyl7 <dimitrisstylianou@proton.me>
2024-02-25 21:41:19 +0200

    Update Project\IncidenceMatrix.py
    - Update how continuous incidence matrix H is calculated.
    
    Co-Authored-By: Niki Dim <79726656+Nikidmp@users.noreply.github.com>
    Co-Authored-By: panagiota02 <79789822+panagiota02@users.noreply.github.com>

--- Project/IncidenceMatrix.py
+++ Project/IncidenceMatrix.py
@@ -3,23 +3,15 @@ import math
 import numpy as np
 
 
-def calculate_continuous_incidence_matrix(N, k, target_indices):
+def calculate_continuous_incidence_matrix(N, k):
     """
     Calculate the continuous incidence matrix H
     :param N: the neighbourhood set matrix N
     :param k: the number of most similar images
-    :param target_indices: the indices of the target images in the subset dataset
     :return: the continuous incidence matrix H
     """
-    H = []
+    H = np.zeros((len(N), len(N)))
     for i in range(len(N)):
-        e = [v for (v, _) in N[i]]
-        h = []
-        for v in target_indices:
-            if v in e:
-                j = e.index(v)
-                h.append(1 - math.log(N[i][j][1], k + 1))
-            else:
-                h.append(0)
-        H.append(h)
-    return np.array(H)
+        for (j, r_i_j) in N[i]:
+            H[i][j] = 1 - math.log(r_i_j, k + 1)
+    return H
