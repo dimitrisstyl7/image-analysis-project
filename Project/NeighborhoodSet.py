c07ef168efdde20290d0ec1e894c323842e020e8
dimitrisstyl7 <dimitrisstylianou@proton.me>
2024-02-27 10:50:13 +0200

    Update NeighborhoodSet.py
    Co-Authored-By: Niki Dim <79726656+Nikidmp@users.noreply.github.com>
    Co-Authored-By: panagiota02 <79789822+panagiota02@users.noreply.github.com>

--- Project/NeighborhoodSet.py
+++ Project/NeighborhoodSet.py
@@ -1,8 +1,8 @@
 def create_neighborhood_set_matrix(T, k):
     """
     This function creates the neighborhood set matrix N
-    :param T: Ranked list of images based on the similarity measures
+    :param T: Ranked list of images based on the weights
     :param k: Number of images to be considered in the neighborhood set
-    :return: A neighborhood set, format (same as T): [[(image_index, similarity_measure), ...], ...]
+    :return: A neighborhood set, format (same as T): [[(image_index, weight), ...], ...]
     """
     return [t[:k] for t in T]
